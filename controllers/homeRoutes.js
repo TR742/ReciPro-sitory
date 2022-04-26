8ff7ae9a183270b5ddf994b7bb9b9bf1c1ac3183
Lawson Vanderpool <lawsonvanderpool@gmail.com>
2022-04-26 11:24:40 -0700

    began making routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,1 +1,9 @@
-qq
+const router = require('express').Router();
+
+router.get('/', async (req, res) => {
+    try {
+      res.render('homepage');
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
