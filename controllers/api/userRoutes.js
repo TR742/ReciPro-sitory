b1715ab1a3fc96aeb4555dd31d07ba39418ce55d
Lawson Vanderpool <lawsonvanderpool@gmail.com>
2022-04-27 14:25:33 -0700

    fixed routesgit add . !

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -4,7 +4,7 @@ const { User } = require('../../models');
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create(req.body);
-
+    
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
@@ -13,6 +13,7 @@ router.post('/', async (req, res) => {
     });
   } catch (err) {
     res.status(400).json(err);
+    console.log(err)
   }
 });
 
