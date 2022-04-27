a93d5d4b1b9dc62aeb9b98f31888de2e725ea657
Brant Martin <brantmartin473@gmail.com>
2022-04-27 12:55:01 -0700

    little changes for models and seed that make things work

--- models/Recipe.js
+++ models/Recipe.js
@@ -1,29 +1,27 @@
-const { Model, DataTypes } = require('sequalize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 class Recipe extends Model {}
 
-Recipe.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        recipe_name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        recipe_id: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        recipe_ingredients: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            },
-        },
-);
+Recipe.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  recipe_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  recipe_id: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  recipe_ingredients: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
 
 module.exports = Recipe;
--- models/User.js
+++ models/User.js
@@ -1,7 +1,12 @@
 const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt')
 const sequelize = require('../config/connection');
 
-class User extends Model {}
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 User.init(
 {
@@ -27,7 +32,7 @@ User.init(
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
-            let: [8],
+            len: [8],
         },
     },
 }, 
